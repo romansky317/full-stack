374bd4466d8d34b8082f76370bda6f7d352b4a47
Roman <romanskycoding@gmail.com>
2020-03-13 09:53:16 +0200

    cx

--- SPA/React/react-redux/redux2/src/index.js
+++ SPA/React/react-redux/redux2/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
